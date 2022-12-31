7c51deb75aa23878bd76ead380d1b1dc2d6e317e
s1101708 <113880951+s1101708@users.noreply.github.com>
2022-12-31 19:49:57 +0800

    Update index.py

--- index.py
+++ index.py
@@ -1,35 +1,19 @@
-import requests, json
-from bs4 import BeautifulSoup
-
-from flask import Flask, render_template, request, make_response, jsonify
-import firebase_admin
-from firebase_admin import credentials, firestore
-
-cred = credentials.Certificate("serviceAccountKey.json")
-firebase_admin.initialize_app(cred)
-db = firestore.client()
-
+from flask import Flask, render_template
+from datetime import datetime
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    homepage = "<h1>楊子青Firestore資料庫存取，提供webhook</h1>"
-    homepage += "<a href=/movie>讀取開眼電影即將上映影片，寫入Firestore</a><br><br>"
-    homepage += "<a href=/query>電影查詢</a><br>"
-    return homepage
+    return "Hello World!"
 
-@app.route("/webhook", methods=["POST"])
-def webhook1():
-    # build a request object
-    req = request.get_json(force=True)
-    # fetch queryResult from json
-    action =  req["queryResult"]["action"]
-    msg =  req["queryResult"]["queryText"]
-    info = "動作：" + action + "； 查詢內容：" + msg
-    return make_response(jsonify({"fulfillmentText": info}))
+@app.route("/mis")
+def course():
+    return "<h1>資訊管理導論</h1>"
 
-
-
+@app.route("/today")
+def today():
+    now = datetime.now()
+    return render_template("today.html", datetime = str(now))
 
 if __name__ == "__main__":
     app.run()
